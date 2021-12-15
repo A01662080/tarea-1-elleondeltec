4d74d36112bfe2a7c96701a60f0f928a428c1192
A01662080 <88857363+A01662080@users.noreply.github.com>
2021-12-15 01:02:42 -0600

    Update test_exercise.py

--- assignments/21LevaduraParaPizza/tests/test_exercise.py
+++ assignments/21LevaduraParaPizza/tests/test_exercise.py
@@ -1,43 +1,10 @@
-import pytest
-import src.exercise
-from tests.input_data import input_values
+def main():
 
-# Define the parametrization based on the inputs from the input_data file
-@pytest.mark.parametrize('value, result, message', input_values)
+    gramos = int(input("Dame la harina en gramos: "))
+    harina  = gramos  *  .050
+    harina = round(harina,1)
+    print("Necesitas estos gramos de levadura:",harina)
 
-def test_exercise(value, result, message):
-    """
-    Tests the code in src.exercise using the inputs from tests.input_data. Asserts that what is printed in the terminal is the same as the expected result.
 
-    Parameters:
-        value: list(str)
-            The values that are used as input for the test. Usually strings, since we are simulating input from user.
-        result: list(str)
-            The expected output in the terminal, usually what the user prints
-        message: str
-            A hint string in case of an error
-    """
-    output = []
-
-    def mock_input(input_s=None):
-        """
-        Create a mock input, where each input is obtained from the list of inputs. Appends the input to a list.
-
-        Parameters:
-            input_s: str
-                Input from the test exercise.
-        """
-        if input_s is not None:
-            output.append(input_s)
-        else:
-            output.append("")
-
-        return value.pop(0)
-
-    src.exercise.input = mock_input
-    #src.exercise.print = lambda s : output.append(s)
-    src.exercise.print = lambda *args: output.append(" ".join(map(str, args)))
-
-    src.exercise.main()
-
-    assert output == result, message
+if __name__ == '__main__':
+    main()
